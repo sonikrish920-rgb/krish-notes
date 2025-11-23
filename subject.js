1370fe905845294fd4a1f8bb81126c6214cf3240
sonikrish920-rgb <sonikrish920@gmail.com>
2025-11-23 14:37:18 +0530

    Update subject.js

--- subject.js
+++ subject.js
@@ -6,7 +6,7 @@ const subjects = {
        chemistry: {
     title: "Engineering Chemistry",
     pdfs: [
-        { name: "Unit 1 Notes", link: "https://github.com/sonikrish920-rgb/krish-notes/blob/main/pdfs/beee/UnitsNotes/unit1.pdf"},
+        { name: "Unit 1 Notes", link: "https://github.com/sonikrish920-rgb/krish-notes/main/pdfs/beee/UnitsNotes/unit1.pdf"},
         { name: "Unit 2 Notes", link: "https://github.com/sonikrish920-rgb/krish-notes/blob/main/pdfs/beee/UnitsNotes/unit2.pdf" },
         { name: "Unit 3 Notes", link: "https://github.com/sonikrish920-rgb/krish-notes/blob/main/pdfs/beee/UnitsNotes/unit3.pdf" },
         { name: "Unit 4 Notes", link: "https://github.com/sonikrish920-rgb/krish-notes/blob/main/pdfs/beee/UnitsNotes/unit4.pdf" },
@@ -218,5 +218,6 @@ if (subjects[sub]) {
     pdfList.innerHTML = "<p>No data found</p>";
 
 }
+
 
 
